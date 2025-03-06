8d7293aed2a55608cce8b69dd80e95e18e5d1e60
Vishal Prajapati <vishalp3212890@gmail.com>
2025-03-06 22:47:32 +0530

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
